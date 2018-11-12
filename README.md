ada36e665ed1536cd1a959c32b61dd18cf03f7bd
Grace Yoo <graceyoo33@gmail.com>
2018-11-11 19:58:09 -0500

    working app

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FriendFinder
+
+Files to update:
+data > friends.js > need to add more ppl
+public > survey.html > need to add js
+routing > apiRoutes.js > need to update post request
+
+Completed files:
+public > home.html
+routing > htmlRoutes.js
+server.js
---
+++ app/data/friends.js
@@ -0,0 +1,132 @@
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://oldnavy.gap.com/webcontent/0011/247/651/cn11247651.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Brian",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNLHPO_B3lwcKaFVJ4RCfNqNr8Txa7CQLhBKiD-ILE41xOeXTnwA",
+        scores: [
+            5,
+            2,
+            4,
+            1,
+            4,
+            2,
+            4,
+            3,
+            4,
+            2
+        ]
+    },
+    {
+        name: "Christopher",
+        photo: "http://www.foodservicecc.com/image/cache/data/category_18/nike-court-dry-tennis-polo-activewear-shirts-men-high-quality-wzejot3d3wevsw-3361-600x600_0.jpg",
+        scores: [
+            4,
+            1,
+            5,
+            3,
+            1,
+            5,
+            1,
+            2,
+            3,
+            1
+        ]
+    },
+    {
+        name: "Matthew",
+        photo: "http://www.face-agency.co.uk/images/uploads/models/large/1470043879-21.jpg",
+        scores: [
+            5,
+            3,
+            4,
+            3,
+            5,
+            1,
+            5,
+            1,
+            3,
+            1
+        ]
+    },
+    {
+        name: "Liam",
+        photo: "https://static.cinemagia.ro/img/db/actor/07/03/15/liam-hemsworth-742879l.jpg",
+        scores: [
+            2,
+            4,
+            3,
+            4,
+            1,
+            1,
+            4,
+            5,
+            3,
+            5
+        ]
+    },
+    {
+        name: "Patrick",
+        photo: "http://www.foto-wallner.at/wp-content/uploads/2015/01/66c297f1dcd085d2304f63c5ed612c0a-beauty-portrait-male-portraits.jpg",
+        scores: [
+            3,
+            5,
+            4,
+            1,
+            5,
+            3,
+            3,
+            1,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Ryan",
+        photo: "http://www.adroitcareer.com.sg/images/Sunglasses/Fashion-Men-Jeepers-Peepers-Round-Glasses-black-Clear-lenses_94.jpg",
+        scores: [
+            4,
+            2,
+            5,
+            5,
+            4,
+            2,
+            1,
+            5,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Travis",
+        photo: "http://www.adroitcareer.com.sg/images/Sunglasses/High-Quality-Men-Jeepers-Peepers-Retro-Glasses-black-Clear-lenses_93.jpg",
+        scores: [
+            3,
+            2,
+            1,
+            2,
+            1,
+            5,
+            3,
+            4,
+            1,
+            5
+        ]
+    }
+];
+
+module.exports = friends;
